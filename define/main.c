610830cab65fa101ed353c0cf0a56f8966d6ff94
slarsar <slarsar@yandex.com>
2019-09-04 17:39:14 +0800

    defile macro

---
+++ define/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("line number is %d\n",__LINE__);
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("%s\n",__FILE__);
+    printf("Hello, World!\n");
+
+    return 0;
+}
