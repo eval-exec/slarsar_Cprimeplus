2ff442d6de31f133d0a3649e1ff2806b8f0d9917
slarsar <slarsar@yandex.com>
2019-09-04 16:14:06 +0800

    argv[0[]

--- pointers/main.c
+++ pointers/main.c
@@ -2,9 +2,11 @@
 #include "stdlib.h"
 
 int main(int argc, char **argv) {
+    printf("argv[0] is %s\n",*argv);
     while (*++argv != NULL) {
         printf("%s\n",*argv);
     }
+    printf("argc is %d\n",argc);
     return EXIT_SUCCESS;
 }
 
