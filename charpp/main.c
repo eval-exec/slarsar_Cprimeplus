a7c065b8fd792abc6c9e34d547b878b510d7b2c7
slarsar <slarsar@yandex.com>
2019-09-04 17:36:59 +0800

    array pointer

--- charpp/main.c
+++ charpp/main.c
@@ -6,10 +6,15 @@ int main() {
     for (i  = 0 ; i < 10; i++ ){
         char x[] = "jackkkkffffffffffffffffffff";
         array[i] = x;
-        printf("address of array[%d]is %p\n",i,&array[i]);
-        printf("size of %ld\n", sizeof(char));
     }
+    char **ptr = array;
+    printf("address of ptr %p\n",ptr);
+    ptr++;
 
-    printf("array is %s",array[0]);
+    printf("ptr is %p\n",ptr);
+
+
+    printf("array[0] is %p\n",&array[0]);
+    printf("array[1] is %p\n",&array[1]);
     return 0;
 }
