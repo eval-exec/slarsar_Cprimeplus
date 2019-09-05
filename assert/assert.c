92ce6152200700a94fbd1211ec8fbb779390ca18
slarsar <slarsar@yandex.com>
2019-09-05 09:45:38 +0800

    assert

---
+++ assert/assert.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <assert.h>
+int main(){
+	int a = 1;
+	assert(a == 0 )	;
+}
---
+++ atexit/atexit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void func1(){
+	printf("exit at 1\n");
+}
+void func2(){
+	printf("exit at 2\n");
+
+}
+
+typedef struct {
+	int a;
+	int b;
+}A;
+
+
+int main(){
+	A ff;
+	ff.
+	
+
+	printf("a1");
+	atexit(func1);
+	atexit(func2);
+	exit(0);
+}
+
